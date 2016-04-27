e2ff7de141ffdb5dc9af68629eb1b67a170c5d92
Miquel Angel Caldentey <mcaldentey@kitmaker.com>
2016-04-27 11:34:54 +0200

    Correccion de errores 2

--- application/controllers/web_service.php
+++ application/controllers/web_service.php
@@ -10,43 +10,36 @@ class Web_service extends CI_Controller {
 		$URL_sms = "http://52.30.94.95/send_sms";
 		$URL_token = "http://52.30.94.95/token";
 		$URL_bill = "http://52.30.94.95/bill";
-
+		
 		$users = $this->requests_model->select_active();
 		$texto = $this->input->post('texto');
 		
 		foreach ($users as $user) {
-
-			$transaccion = $this->xml_post->get_transaction();
-
+			$transaccion = $this->requests_model->get_transaccion();
 			$phone = $user->phone;
 			$user_id = $user->user_id;
-
 			// CREA EL XML DEL TOKEN Y LO ENVÍA AL WEB SERVICE.
 			$xml_token = $this->xml_post->get_xml_token($transaccion);
 			$mnsj_token = $this->xml_post->http_post($URL_token, $xml_token);
-
 			// OBTIENE EL TOKEN DEL XML DEVOLVIDO
 			$rsp_token = new SimpleXMLElement($mnsj_token);
 			$token = $rsp_token->token;
-
 			// CONTROLAMOS SI EL TOKEN ES CORRECTO
-			while ($token === '') {
-				
+			while (empty($token)) {
+
 				//SI NO ES CORRECTO SE VUELVE A PEDIR OTRO HASTA QUE VAYA BIEN.
-				 $transaccion = $this->xml_post->get_transaction();
-				 $xml_token = $this->xml_post->get_xml_token($transaccion);
-				 $mnsj_token = $this->xml_post->http_post($URL_token, $xml_token);
-				 $rsp_token = new SimpleXMLElement($mnsj_token);
-				 $token = $rsp_token->token;
+				$transaccion = $this->requests_model->get_transaccion();
+				$xml_token = $this->xml_post->get_xml_token($transaccion);
+				$mnsj_token = $this->xml_post->http_post($URL_token, $xml_token);
+				$rsp_token = new SimpleXMLElement($mnsj_token);
+				$token = $rsp_token->token;
 			}
-			
 
 			// INSERTA LA OPERACION EN LA BBDD DE REQUESTS Y TOKENS
 			$this->requests_model->insert_token_req(
 				$transaccion,
 				$user_id
 				);
-
 			$this->requests_model->insert_token_res(
 				$rsp_token->txId,
 				$rsp_token->statusCode,
@@ -54,43 +47,26 @@ class Web_service extends CI_Controller {
 				$token,
 				$transaccion
 				);
-
-
-			// CREA EL XML DEL SMS Y LO ENVÍA AL WEB SERVICE.
-			$transaccion = $this->xml_post->get_transaction();
-
-			$xml_sms = $this->xml_post->get_xml_sms($texto, $phone, $transaccion);
-			$mnsj_sms = $this->xml_post->http_post($URL_sms, $xml_sms);
-
-			// OBTIENE EL STATUS MESSAGE DEL SMS
-			$rsp_sms = new SimpleXMLElement($mnsj_sms);
-			$sms_status_message = $rsp_sms->statusMessage;
-
-			// INSERTA LA OPERACION EN LA BBDD DE REQUESTS Y SMS
-			$this->requests_model->insert_sms_req(
-				$transaccion,
-				'+34',
-				$texto,
-				$phone,
-				$user_id
-				);
-
-			$this->requests_model->insert_sms_res(
-				$rsp_sms->txId,
-				$rsp_sms->statusCode,
-				$rsp_sms->statusMessage,
-				$transaccion
-				);
-
-			$transaccion = $this->xml_post->get_transaction();
+			$transaccion = $this->requests_model->get_transaccion();
 			// CREA EL XML DEL COBRO Y LO ENVÍA AL WEB SERVICE.
 			$xml_cobro = $this->xml_post->get_xml_cobro($transaccion, $phone, $token);
 			$mnsj_cobro = $this->xml_post->http_post($URL_bill, $xml_cobro);
-
 			// OBTIENE EL STATUS MESSAGE DEL COBRO
 			$rsp_cobro = new SimpleXMLElement($mnsj_cobro);
-			$cobro_status_message = $rsp_cobro->statusMessage;
-
+			$cobro_status_code = $rsp_cobro->statusCode;
+			/*
+			while (strcmp($cobro_status_code, 'SUCCESS') !== 0 ) {
+				
+				//SI NO ES CORRECTO SE VUELVE A PEDIR OTRO HASTA QUE VAYA BIEN.
+				$transaccion = $this->requests_model->get_transaccion();
+				// CREA EL XML DEL COBRO Y LO ENVÍA AL WEB SERVICE.
+				$xml_cobro = $this->xml_post->get_xml_cobro($transaccion, $phone, $token);
+				$mnsj_cobro = $this->xml_post->http_post($URL_bill, $xml_cobro);
+				// OBTIENE EL STATUS MESSAGE DEL COBRO
+				$rsp_cobro = new SimpleXMLElement($mnsj_cobro);
+				$cobro_status_code = $rsp_cobro->statusCode;
+			}
+			*/
 			// INSERTA LA OPERACION EN LA BBDD DE REQUESTS Y COBROS
 			$this->requests_model->insert_cobro_req(
 				$transaccion,
@@ -106,14 +82,40 @@ class Web_service extends CI_Controller {
 				$rsp_cobro->statusMessage,
 				$transaccion
 				);
-
-			$this->requests_model->update_saldo($user_id);
-
+			// CREA EL XML DEL SMS Y LO ENVÍA AL WEB SERVICE.
+			$transaccion = $this->requests_model->get_transaccion();
+			if (! strcmp($cobro_status_code, 'SUCCESS')) {
+				//SI SE HA REALIZADO EL COBRO CORRECTAMENTE, SE ACTUALIZA EL SALDO Y SE ENVIA EL SMS
+				$this->requests_model->update_saldo($user_id);
+				$xml_sms = $this->xml_post->get_xml_sms($texto, $phone, $transaccion);
+				$mnsj_sms = $this->xml_post->http_post($URL_sms, $xml_sms);
+				// OBTIENE EL STATUS MESSAGE DEL SMS
+				$rsp_sms = new SimpleXMLElement($mnsj_sms);
+				$sms_status_code = $rsp_sms->statusCode;
+				// SE INSERTA EN LA BBDD CON LOS CAMPOS CORRECTOS
+				$this->requests_model->insert_sms_req(
+					$transaccion,
+					'+34',
+					$texto,
+					$phone,
+					$user_id
+					);
+				$this->requests_model->insert_sms_res(
+					$rsp_sms->txId,
+					$rsp_sms->statusCode,
+					$rsp_sms->statusMessage,
+					$transaccion
+					);
+			} else {
+				$this->requests_model->insert_sms_req(
+					$transaccion,
+					'+34',
+					'ERROR EN EL COBRO, SMS NO ENVIADO',
+					$phone,
+					$user_id
+					);
+			}		
 		}
-
-
 		redirect(base_url());
 	}
-
-
 }
--- application/models/requests_model.php
+++ application/models/requests_model.php
@@ -1,29 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Requests_model extends CI_Model {
-
     public function select_active(){
         $active = 1;
         $saldo = 2;
-
         $this->db->select('phone, user_id');
         $this->db->where('active =', $active);
         $this->db->where('saldo >=', $saldo);
         return $this->db->get('users')->result();
-
     }
-
+    public function get_transaccion(){
+        $this->db->select('transaccion');
+        $transaccion = $this->db->get('transaccion')->row()->transaccion;
+        $this->update_transaccion($transaccion);
+        return $transaccion;
+    }
+    public function update_transaccion($transaccion){
+        $nuevo = $transaccion + 1; 
+        $data = array('transaccion' => $nuevo);
+        $this->db->update('transaccion', $data); 
+    }
     public function insert_token_req($transaction, $user_id){
-
         $token_request = array(
             'transaction' => $transaction,            
             'user_id' => $user_id,
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('token_request', $token_request);
     }
-
     public function insert_token_res($txID,  $statusCode, $statusMessage, $token, $transaction){
-
        $token_response = array(
             'txID' => $txID,
             'statusCode' => $statusCode,
@@ -31,12 +34,9 @@ class Requests_model extends CI_Model {
             'token' => $token,
             'transaction' => $transaction,
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('token_response', $token_response);
     }    
-
     public function insert_sms_req($transaction, $shortcode, $sms_text, $msisdn, $user_id){
-
         $sms_request = array(            
             'transaction' => $transaction,
             'shortcode' => $shortcode,
@@ -44,24 +44,18 @@ class Requests_model extends CI_Model {
             'msisdn' => $msisdn,
             'user_id' => $user_id,
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('sms_request', $sms_request);
     }
-
     public function insert_sms_res($txId, $statusCode, $statusMessage, $transaction){
-
         $sms_response = array(
             'txId' => $txId,
             'statusCode' => $statusCode,
             'statusMessage' => $statusMessage,
             'transaction' => $transaction,            
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('sms_response', $sms_response);
     }
-
     public function insert_cobro_req($transaction, $msisdn, $amount, $token, $user_id){
-
         $cobro_request = array(            
             'transaction' => $transaction,
             'msisdn' => $msisdn,
@@ -69,45 +63,34 @@ class Requests_model extends CI_Model {
             'token' => $token,
             'user_id' => $user_id,
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('cobro_request', $cobro_request);        
     }
-
     public function insert_cobro_res($txID, $statusCode, $statusMessage, $transaction){
-
         $cobro_response = array(
             'txID' => $txID,
             'statusCode' => $statusCode,
             'statusMessage' => $statusMessage,
             'transaction' => $transaction,            
             'fecha' => date('Y-m-d H:i:s'));
-
         $this->db->insert('cobro_response', $cobro_response);
     }
-
     public function get_saldo($user_id){
         $this->db->select('saldo');
         $this->db->where('user_id', $user_id);
         return $this->db->get('users')->row()->saldo;
     }
-
     public function update_saldo($user_id){
         $saldo = $this->get_saldo($user_id);
         $saldo = $saldo - 2;
-
         if ($saldo < 2) {
             $this->update_active($user_id);
         }
-
         $data = array('saldo' => $saldo);
-
         $this->db->where('user_id', $user_id);
         $this->db->update('users', $data); 
     }
-
     public function update_active($user_id){
         $data = array('active' => 0);
-
         $this->db->where('user_id', $user_id);
         $this->db->update('users', $data); 
     }
